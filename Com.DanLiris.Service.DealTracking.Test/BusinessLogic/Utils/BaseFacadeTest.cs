242787484aef5c540a2e986c6556f712bbdc4bf0
adepamungkas <ade.pamungkas9@gmail.com>
2020-05-15 07:49:31 +0700

    Unit Test BoardLogic

--- Com.DanLiris.Service.DealTracking.Test/BusinessLogic/Utils/BaseFacadeTest.cs
+++ Com.DanLiris.Service.DealTracking.Test/BusinessLogic/Utils/BaseFacadeTest.cs
@@ -14,135 +14,135 @@ using Xunit;
 
 namespace Com.DanLiris.Service.DealTracking.Test.BusinessLogic.Utils
 {
-    //public abstract class BaseFacadeTest<TDbContext, TFacade, TLogic, TModel, TDataUtil>
-    //    where TDbContext : StandardDbContext
-    //    where TFacade : class, IBaseFacade<TModel>
-    //    where TLogic : BaseLogic<TModel>
-    //    where TModel : BaseModel
-    //    where TDataUtil : BaseDataUtil<TFacade, TModel>
-    //{
-    //    private string _entity;
+    public abstract class BaseFacadeTest<TDbContext, TFacade, TLogic, TModel, TDataUtil>
+        where TDbContext : StandardDbContext
+        where TFacade : class, IBaseFacade<TModel>
+        where TLogic : BaseLogic<TModel>
+        where TModel : BaseModel
+        where TDataUtil : BaseDataUtil<TFacade, TModel>
+    {
+        private string _entity;
 
-    //    public BaseFacadeTest(string entity)
-    //    {
-    //        _entity = entity;
-    //    }
+        public BaseFacadeTest(string entity)
+        {
+            _entity = entity;
+        }
 
-    //    [MethodImpl(MethodImplOptions.NoInlining)]
-    //    protected string GetCurrentMethod()
-    //    {
-    //        StackTrace st = new StackTrace();
-    //        StackFrame sf = st.GetFrame(1);
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected string GetCurrentMethod()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
 
-    //        return string.Concat(sf.GetMethod().Name, "_", _entity);
-    //    }
+            return string.Concat(sf.GetMethod().Name, "_", _entity);
+        }
 
-    //    protected TDbContext DbContext(string testName)
-    //    {
-    //        DbContextOptionsBuilder<TDbContext> optionsBuilder = new DbContextOptionsBuilder<TDbContext>();
-    //        optionsBuilder
-    //            .UseInMemoryDatabase(testName)
-    //            .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+        protected TDbContext DbContext(string testName)
+        {
+            DbContextOptionsBuilder<TDbContext> optionsBuilder = new DbContextOptionsBuilder<TDbContext>();
+            optionsBuilder
+                .UseInMemoryDatabase(testName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning));
 
-    //        TDbContext dbContext = Activator.CreateInstance(typeof(TDbContext), optionsBuilder.Options) as TDbContext;
+            TDbContext dbContext = Activator.CreateInstance(typeof(TDbContext), optionsBuilder.Options) as TDbContext;
 
-    //        return dbContext;
-    //    }
+            return dbContext;
+        }
 
-    //    protected virtual Mock<IServiceProvider> GetServiceProviderMock(TDbContext dbContext)
-    //    {
-    //        var serviceProviderMock = new Mock<IServiceProvider>();
+        protected virtual Mock<IServiceProvider> GetServiceProviderMock(TDbContext dbContext)
+        {
+            var serviceProviderMock = new Mock<IServiceProvider>();
 
-    //        IIdentityService identityService = new IdentityService { Username = "Username" };
+            IIdentityService identityService = new IdentityService { Username = "Username" };
 
-    //        serviceProviderMock
-    //            .Setup(x => x.GetService(typeof(IdentityService)))
-    //            .Returns(identityService);
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IdentityService)))
+                .Returns(identityService);
 
-    //        serviceProviderMock
-    //            .Setup(x => x.GetService(typeof(TLogic)))
-    //            .Returns(Activator.CreateInstance(typeof(TLogic), identityService, dbContext) as TLogic);
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(TLogic)))
+                .Returns(Activator.CreateInstance(typeof(TLogic), identityService, dbContext) as TLogic);
 
-    //        return serviceProviderMock;
-    //    }
+            return serviceProviderMock;
+        }
 
-    //    protected virtual TDataUtil DataUtil(TFacade facade, TDbContext dbContext = null)
-    //    {
-    //        TDataUtil dataUtil = Activator.CreateInstance(typeof(TDataUtil), facade) as TDataUtil;
-    //        return dataUtil;
-    //    }
+        protected virtual TDataUtil DataUtil(TFacade facade, TDbContext dbContext = null)
+        {
+            TDataUtil dataUtil = Activator.CreateInstance(typeof(TDataUtil), facade) as TDataUtil;
+            return dataUtil;
+        }
 
-    //    [Fact]
-    //    public virtual async void Create_Success()
-    //    {
-    //        var dbContext = DbContext(GetCurrentMethod());
-    //        var serviceProvider = GetServiceProviderMock(dbContext).Object;
+        [Fact]
+        public virtual async void Create_Success()
+        {
+            var dbContext = DbContext(GetCurrentMethod());
+            var serviceProvider = GetServiceProviderMock(dbContext).Object;
 
-    //        TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
+            TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
 
-    //        var data = await DataUtil(facade, dbContext).GetNewData();
+            var data = await DataUtil(facade, dbContext).GetNewData();
 
-    //        var response = await facade.Create(data);
+            var response = await facade.Create(data);
 
-    //        Assert.NotEqual(response, 0);
-    //    }
+            Assert.NotEqual(response, 0);
+        }
 
-    //    [Fact]
-    //    public virtual async void Get_All_Success()
-    //    {
-    //        var dbContext = DbContext(GetCurrentMethod());
-    //        var serviceProvider = GetServiceProviderMock(dbContext).Object;
+        [Fact]
+        public virtual async void Get_All_Success()
+        {
+            var dbContext = DbContext(GetCurrentMethod());
+            var serviceProvider = GetServiceProviderMock(dbContext).Object;
 
-    //        TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
+            TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
 
-    //        var data = await DataUtil(facade, dbContext).GetTestData();
+            var data = await DataUtil(facade, dbContext).GetTestData();
 
-    //        var Response = facade.Read(1, 25, "{}", new List<string>(), "", "{}");
+            var Response = facade.Read(1, 25, "{}", new List<string>(), "", "{}");
 
-    //       // Assert.NotEqual(Response.Data.Count, 0);
-    //    }
+            // Assert.NotEqual(Response.Data.Count, 0);
+        }
 
-    //    [Fact]
-    //    public virtual async void Get_By_Id_Success()
-    //    {
-    //        var dbContext = DbContext(GetCurrentMethod());
-    //        var serviceProvider = GetServiceProviderMock(dbContext).Object;
+        [Fact]
+        public virtual async void Get_By_Id_Success()
+        {
+            var dbContext = DbContext(GetCurrentMethod());
+            var serviceProvider = GetServiceProviderMock(dbContext).Object;
 
-    //        TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
+            TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
 
-    //        var data = await DataUtil(facade, dbContext).GetTestData();
+            var data = await DataUtil(facade, dbContext).GetTestData();
 
-    //        var Response = facade.ReadById((int)data.Id);
+            var Response = facade.ReadById((int)data.Id);
 
-    //        Assert.NotEqual(Response.Id, 0);
-    //    }
+            Assert.NotEqual(Response.Id, 0);
+        }
 
-    //    [Fact]
-    //    public virtual async void Update_Success()
-    //    {
-    //        var dbContext = DbContext(GetCurrentMethod());
-    //        var serviceProvider = GetServiceProviderMock(dbContext).Object;
+        [Fact]
+        public virtual async void Update_Success()
+        {
+            var dbContext = DbContext(GetCurrentMethod());
+            var serviceProvider = GetServiceProviderMock(dbContext).Object;
 
-    //        TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
+            TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
 
-    //        var data = await DataUtil(facade, dbContext).GetTestData();
+            var data = await DataUtil(facade, dbContext).GetTestData();
 
-    //        var response = await facade.Update((int)data.Id, data);
+            var response = await facade.Update((int)data.Id, data);
 
-    //        Assert.NotEqual(response, 0);
-    //    }
+            Assert.NotEqual(response, 0);
+        }
 
-    //    [Fact]
-    //    public virtual async void Delete_Success()
-    //    {
-    //        var dbContext = DbContext(GetCurrentMethod());
-    //        var serviceProvider = GetServiceProviderMock(dbContext).Object;
+        [Fact]
+        public virtual async void Delete_Success()
+        {
+            var dbContext = DbContext(GetCurrentMethod());
+            var serviceProvider = GetServiceProviderMock(dbContext).Object;
 
-    //        TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
-    //        var data = await DataUtil(facade, dbContext).GetTestData();
+            TFacade facade = Activator.CreateInstance(typeof(TFacade), serviceProvider, dbContext) as TFacade;
+            var data = await DataUtil(facade, dbContext).GetTestData();
 
-    //        var Response = await facade.Delete((int)data.Id);
-    //        Assert.NotEqual(Response, 0);
-    //    }
-    //}
+            var Response = await facade.Delete((int)data.Id);
+            Assert.NotEqual(Response, 0);
+        }
+    }
 }
