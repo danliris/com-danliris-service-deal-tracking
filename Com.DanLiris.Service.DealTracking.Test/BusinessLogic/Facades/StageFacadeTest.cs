19e11a52b25833d0df8152371b07e45eefc0fb4c
adepamungkas <ade.pamungkas9@gmail.com>
2020-05-15 15:37:02 +0700

    Unit Test stage

--- Com.DanLiris.Service.DealTracking.Test/BusinessLogic/Facades/StageFacadeTest.cs
+++ Com.DanLiris.Service.DealTracking.Test/BusinessLogic/Facades/StageFacadeTest.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using Com.DanLiris.Service.DealTracking.Lib;
+using Com.DanLiris.Service.DealTracking.Lib.BusinessLogic.Facades;
+using Com.DanLiris.Service.DealTracking.Lib.BusinessLogic.Implementation;
+using Com.DanLiris.Service.DealTracking.Lib.Models;
+using Com.DanLiris.Service.DealTracking.Lib.Services;
+using Com.DanLiris.Service.DealTracking.Test.BusinessLogic.DataUtils;
+using Com.DanLiris.Service.DealTracking.Test.BusinessLogic.Utils;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Com.DanLiris.Service.DealTracking.Test.BusinessLogic.Facades
 {
-    class StageFacadeTest
+  
+    public class StageFacadeTest : BaseFacadeTest<DealTrackingDbContext, StageFacade, StageLogic, Stage, StageDataUtil>
     {
+        private const string ENTITY = "DealTrackingStages";
+        public StageFacadeTest():base(ENTITY)
+        {
+
+        }
+
+        protected override Mock<IServiceProvider> GetServiceProviderMock(DealTrackingDbContext dbContext)
+        {
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            IIdentityService identityService = new IdentityService { Username = "Username", Token = "TokenUnitTest" };
+            serviceProviderMock
+               .Setup(x => x.GetService(typeof(IdentityService)))
+               .Returns(identityService);
+
+            var stageLogic = new StageLogic(serviceProviderMock.Object, dbContext);
+
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(StageLogic)))
+                .Returns(stageLogic);
+
+
+
+            return serviceProviderMock;
+        }
     }
 }
