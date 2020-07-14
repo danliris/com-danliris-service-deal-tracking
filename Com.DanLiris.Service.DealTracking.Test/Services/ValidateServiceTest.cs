69abab5fc51980d993bd8e0126c64f22afecf42d
adepamungkas <ade.pamungkas9@gmail.com>
2020-07-14 21:12:53 +0700

    Update ValidateServiceTest.cs

--- Com.DanLiris.Service.DealTracking.Test/Services/ValidateServiceTest.cs
+++ Com.DanLiris.Service.DealTracking.Test/Services/ValidateServiceTest.cs
@@ -12,13 +12,35 @@ namespace Com.DanLiris.Service.DealTracking.Test.Services
    public class ValidateServiceTest
     {
         [Fact]
-        public void Should_Success_Instantiate()
+        public void Validate_Throws_Exception()
         {
             Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
             ValidateService service = new ValidateService(serviceProviderMock.Object);
             StageViewModel stage = new StageViewModel();
             Assert.Throws<ServiceValidationException>(() => service.Validate(stage));
            
+        }
+
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
+            ValidateService service = new ValidateService(serviceProviderMock.Object);
+            StageViewModel stage = new StageViewModel()
+            {
+                Active =true,
+                BoardId=1,
+                Code = "Code",
+                Deals =new List<StageDealViewModel>()
+                {
+                    new StageDealViewModel()
+                },
+                DealsOrder = "DealsOrder",
+                Name ="Name",
+                
+            };
+            service.Validate(stage);
+
         }
     }
 }
