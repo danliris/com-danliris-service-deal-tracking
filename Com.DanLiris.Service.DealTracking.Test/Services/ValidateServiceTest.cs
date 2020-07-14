7710a5dc834a575c56a831b8494cee92f1ab4e11
adepamungkas <ade.pamungkas9@gmail.com>
2020-07-14 20:59:44 +0700

    ValidateServiceTest

---
+++ Com.DanLiris.Service.DealTracking.Test/Services/ValidateServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Com.DanLiris.Service.DealTracking.Lib.Services;
+using Com.DanLiris.Service.DealTracking.Lib.Utilities;
+using Com.DanLiris.Service.DealTracking.Lib.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.DealTracking.Test.Services
+{
+   public class ValidateServiceTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
+            ValidateService service = new ValidateService(serviceProviderMock.Object);
+            StageViewModel stage = new StageViewModel();
+            Assert.Throws<ServiceValidationException>(() => service.Validate(stage));
+           
+        }
+    }
+}
